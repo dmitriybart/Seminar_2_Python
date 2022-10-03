c845f2cecf7f899f85a6690cb1dba45b36a2d351
Dmitriy B <kupperfeet@gmail.com>
2022-10-03 19:26:12 +0300

    task 1 done

--- task_1.py
+++ task_1.py
@@ -1,11 +1,11 @@
 # 1. Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
-n = int(input('Введите чиссло: ')
+n = int(input('Введите число: '))
 element = 1
 print(element, end = " ")
-for i range (n-1):
+for i in range(1,n+3,2):
     if i%2 == 0:
         element *=3
         print(element, end=" ")
     else:
-        element *= -3
+        element *=-3
         print(element, end=" ")
